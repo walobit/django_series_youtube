8870d4188acac70817772c1b84f2c494f709923f
LeOndaz <ahmeddark369@gmail.com>
2019-11-21 19:54:12 +0200

    second video

--- tasks/views.py
+++ tasks/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def TasksHome(request):
-    return render(request, 'tasks/index.html')
+    data = {
+        'mycounter': range(5),
+        'name': 'leondaz',
+        'longword': 'this channel is leondaz'
+    }
+    return render(request, 'tasks/index.html', data)
